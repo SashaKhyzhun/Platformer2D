40014b38fc1eea8b94a8303a52ffda9d07f9b728
Blind56 <petrinko6@gmail.com>
2015-10-07 22:49:26 +0300

    Refactor

--- Assets/Scripts/CameraMovement.cs
+++ Assets/Scripts/CameraMovement.cs
@@ -3,27 +3,80 @@
 public class CameraMovement : MonoBehaviour {
 
     public GameObject player;
-    public float maxSpeed = 1;
+    public float targetSpeed = 1;
     public float accelerationTime = 1;
   
-    private InputController iController;
+    private InputController inputController;
+    //private Rigidbody2D rb;
+    private Vector2 direction;
+    private float xPlayerDeltaPosition;
+    private float xLastPosition;
+    private float xCurrentPosition;
+    private float _targetSpeed;
     private float speed = 0;
+    private bool canChange = true;
 
     void Awake ()
     {
-        iController = player.GetComponent<InputController>();
+        inputController = player.GetComponent<InputController>();
+        //rb = player.GetComponent<Rigidbody2D>();
+    }
+
+    void Start()
+    {
+        _targetSpeed = targetSpeed;
     }
 
     void FixedUpdate()
     {
-        if (iController.start)
+        if (inputController.start)
         {
-            if (speed <= maxSpeed)
+            /*if (speed != targetSpeed)
             {
-                speed = Mathf.Lerp(0, maxSpeed, Time.time * accelerationTime);
+                speed = Mathf.Lerp(0, targetSpeed, Time.time * accelerationTime);
+            }*/
+
+            speed = ChangeSpeed(speed, _targetSpeed);
+
+            xCurrentPosition = Camera.main.WorldToScreenPoint(player.transform.position).x;
+            xPlayerDeltaPosition = xCurrentPosition - xLastPosition;
+
+            //Debug.Log("Delta: " + xPlayerDeltaPosition);
+            //Debug.Log("Speed: " + speed);
+            //Debug.Log("TargetSpeed: " + targetSpeed);
+            //if ()
+            direction = Vector2.right * speed;
+            
+            if (xCurrentPosition >= Screen.width / 2)
+            {
+                //direction += new Vector2(xCurrentPosition - (Screen.width / 2), direction.y);
+                _targetSpeed += 0.01f;
+                canChange = true;
             }
-            Vector2 _direction = Vector2.right * speed;
-            gameObject.transform.Translate(_direction);
+            else
+            {
+                if (canChange)
+                {
+                    if (_targetSpeed > targetSpeed)
+                    {
+                        _targetSpeed = targetSpeed;
+                        canChange = false;
+                    }
+                }
+            }
+            
+            gameObject.transform.Translate(direction);
+
+            xLastPosition = xCurrentPosition;
+        }  
+    }
+
+    private float ChangeSpeed(float currenSpeed, float targetSpeed)
+    {
+        if (currenSpeed != targetSpeed)
+        {
+            currenSpeed = Mathf.Lerp(0, targetSpeed, Time.time * accelerationTime);
         }
+        return currenSpeed;
     }
 }
--- Assets/Scripts/PlayerMotor.cs
+++ Assets/Scripts/PlayerMotor.cs
@@ -4,20 +4,42 @@
 
 public class PlayerMotor : MonoBehaviour {
 
-    public float xForce = 1;
-    public float yForce = 1;
+    public float xForce;
+    public float yForce;
+    public float xMaxSpeed;
+    public float yMaxSpeed;
 
     private Rigidbody2D rb;
 
     void Awake ()
     {
         rb = GetComponent<Rigidbody2D>();
-        rb.freezeRotation = true;
+        //rb.freezeRotation = true;
     }
 
     public void Jump()
     {
-        Vector2 _force = new Vector2(xForce , yForce);
+        Vector2 _force = new Vector2();
+
+        Vector2 _velocity = rb.velocity;
+
+        if (_velocity.x < xMaxSpeed || _velocity. y < yMaxSpeed)
+        {
+            _force = new Vector2(xForce, yForce);
+        }
+        else if (_velocity.x > xMaxSpeed)
+        {
+            _force = new Vector2(0, yForce);
+        }
+        else if (_velocity.y > yMaxSpeed)
+        {
+            _force = new Vector2(xForce, 0);
+        }
+        else
+        {
+            _force = new Vector2(0, 0);
+        }
+        
         rb.AddForce(_force, ForceMode2D.Force);
     }
 

1fc98ed9ce751d73c2b102e6e0c1e279cf4c96ef
Blind56 <petrinko6@gmail.com>
2015-10-11 19:57:13 +0300

    Scene

--- Assets/Scripts/MapManager.cs
+++ Assets/Scripts/MapManager.cs
@@ -4,38 +4,49 @@ using System.Collections;
 public class MapManager : MonoBehaviour {
 
     public GameObject[] mapSegments;
-    public GameObject[] backgroundPrefab;
+    public GameObject[] backgroundSegments;
     public float offset;
 
     private Vector3 camPos;
-    private float[] extents;
+    private float[] mapExtents;
+    private float[] bgExtents;
     private float camExtent;
 
-	// Use this for initialization
 	void Start () {
         offset = Mathf.Abs(offset);
-        extents = new float[mapSegments.Length];
+        mapExtents = new float[mapSegments.Length];
+        bgExtents = new float[backgroundSegments.Length];
         Camera _camera = Camera.main;
         camExtent = _camera.orthographicSize * _camera.aspect;
         for (int i = 0; i < mapSegments.Length; i++)
         {
-            extents[i] = mapSegments[i].GetComponent<Renderer>().bounds.extents.x;
-            //Debug.Log(extents[i]);
+            mapExtents[i] = mapSegments[i].GetComponent<Renderer>().bounds.extents.x;
         }
-	}
+        for (int i = 0; i < backgroundSegments.Length; i++)
+        {
+            bgExtents[i] = backgroundSegments[i].GetComponent<Renderer>().bounds.extents.x;
+            Debug.Log(bgExtents[i]);
+        }
+    }
 	
-	// Update is called once per frame
 	void Update () {
+        ManageMap();
+        ManageBackground();
+	}
+
+    void ManageMap()
+    {
         camPos = Camera.main.transform.position;
-        for (int i = 1; i < extents.Length; i++) {
-            if (camPos.x + camExtent + offset >= mapSegments[i - 1].transform.position.x + extents[i])
+        for (int i = 1; i < mapExtents.Length; i++)
+        {
+            if (camPos.x + camExtent + offset >= mapSegments[i - 1].transform.position.x + mapExtents[i])
             {
                 if (!mapSegments[i].activeInHierarchy)
                 {
                     mapSegments[i].SetActive(true);
                 }
             }
-            if (camPos.x - camExtent - offset >= mapSegments[i].transform.position.x - extents[i])
+            if (camPos.x - camExtent - offset >= mapSegments[i].transform.position.x - mapExtents[i])
             {
                 if (mapSegments[i - 1].activeInHierarchy)
                 {
@@ -43,6 +54,29 @@ public class MapManager : MonoBehaviour {
                 }
             }
         }
+    }
 
-	}
+    void ManageBackground()
+    {
+        /*
+        camPos = Camera.main.transform.position;
+        for (int i = 1; i < backgroundPrefab.Length; i++)
+        {
+            if (camPos.x + camExtent + offset >= mapSegments[i - 1].transform.position.x + mapExtents[i])
+            {
+                if (!mapSegments[i].activeInHierarchy)
+                {
+                    mapSegments[i].SetActive(true);
+                }
+            }
+            if (camPos.x - camExtent - offset >= mapSegments[i].transform.position.x - mapExtents[i])
+            {
+                if (mapSegments[i - 1].activeInHierarchy)
+                {
+                    mapSegments[i - 1].SetActive(false);
+                }
+            }
+        }
+        */
+    }
 }

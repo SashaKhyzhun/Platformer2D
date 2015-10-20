d8be5d6327e176fca549933892c95fa7cd6e8d94
SashaKhyzhun <sasha6427@gmail.com>
2015-10-20 20:39:37 +0300

    bounce + wind

---
+++ Assets/Scripts/Wind.cs
@@ -0,0 +1,32 @@
+﻿using UnityEngine;
+    
+public class Wind : MonoBehaviour {
+
+    public float windForce; 
+
+    private Rigidbody2D rb;
+    
+    void OnTriggerEnter2D(Collider2D collider)
+    {
+        rb = collider.gameObject.GetComponent<Rigidbody2D>();
+    }
+
+
+    void OnTriggerStay2D (Collider2D collider)
+    {
+        if (rb != null)
+        {
+            rb.AddForce(transform.right * windForce * Time.deltaTime);
+        }    
+
+        //Debug.Log(collider.name);
+
+    }
+
+
+    
+
+    
+
+
+}

673858836006b05524548f414dd315025989f604
SashaKhyzhun <sasha6427@gmail.com>
2015-11-05 22:16:39 +0200

    bomb done

---
+++ Assets/Scripts/Bomb.cs
@@ -0,0 +1,27 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Bomb : MonoBehaviour {
+
+    public float boomForce;
+
+    private Transform myTransform;
+    private Rigidbody2D playerRb;
+
+    void Awake()
+    {
+        myTransform = transform;
+    }
+
+    void OnCollisionEnter2D(Collision2D coll)
+    {
+        if (coll.gameObject.tag == "Player")
+        {
+            Vector3 direction = (coll.transform.position - myTransform.position).normalized;
+            playerRb = coll.gameObject.GetComponent<Rigidbody2D>();
+            playerRb.AddForce(direction * boomForce * Time.fixedDeltaTime, ForceMode2D.Impulse);
+        }
+    }
+  
+
+}

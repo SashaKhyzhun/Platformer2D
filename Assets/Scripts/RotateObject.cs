f66f2286d0ece358ba948bcfbcec929538e86aa9
SashaKhyzhun <sasha6427@gmail.com>
2015-11-16 03:30:00 +0200

    удалив анімації, спрайти, +нові спрайти, поміняв нові на старі, подправив карту, нові префаби, убрав лишній мусор ...
    + трохи поміняв Ельвіна спрайти, забрав трохи кривих ліній на фоні і
    всяка куча маленьких змін

---
+++ Assets/Scripts/RotateObject.cs
@@ -0,0 +1,12 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class RotateObject : MonoBehaviour
+{
+   
+    void FixedUpdate()
+    {
+        GetComponent<Rigidbody>().rotation = Quaternion.identity;
+    }
+
+}

bf8f2753ae9a7608f8565f5ffcc203b743b57fb8
SashaKhyzhun <sasha6427@gmail.com>
2015-12-31 21:48:49 +0200

    Пушка, наводка, стрельба, клінер + префаби цього всього є. З наступаючим, всі діла

---
+++ Assets/Scripts/BulletMover.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class BulletMover : MonoBehaviour {
+
+    public GameObject bullet;
+
+    float bulletSpeed = 5f;
+	
+	void Update () {
+
+        Vector3 pos = transform.position;
+        Vector3 velocity = new Vector3(0, bulletSpeed * Time.deltaTime, 0);
+        pos += transform.rotation * velocity;
+        transform.position = pos;
+
+        if (Input.GetKey(KeyCode.Space)) {
+            bullet.transform.Translate(new Vector3(0, Time.deltaTime * bulletSpeed, 0));
+        }
+    }
+
+}
---
+++ Assets/Scripts/FacesPlayer.cs
@@ -0,0 +1,37 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class FacesPlayer : MonoBehaviour {
+
+	public float rotSpeed = 90f;
+
+	Transform player;
+
+	
+	void Update () {
+		if(player == null) {
+			// Find the player
+			GameObject go = GameObject.FindWithTag ("Player");
+
+			if(go != null) {
+				player = go.transform;
+			}
+		}
+
+		// Перевіряєм чи є у нас плеєр на екрані прям зараз;
+
+		if(player == null)
+			return;	// іф нет - перевіряти наступний фрейм;
+
+		// тут ми знаєм точно, шо у нас є 
+		Vector3 dir = player.position - transform.position;
+		dir.Normalize();
+
+		float zAngle = Mathf.Atan2(dir.y, dir.x) * Mathf.Rad2Deg - 90;
+
+		Quaternion desiredRot = Quaternion.Euler( 0, 0,zAngle );
+
+		transform.rotation = Quaternion.RotateTowards( transform.rotation, desiredRot, rotSpeed * Time.deltaTime);
+
+	}
+}

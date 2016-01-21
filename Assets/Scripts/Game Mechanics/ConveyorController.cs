c6b7654e045784530e773418b4d55bad3b1a610e
Blind56 <petrinko6@gmail.com>
2016-01-21 22:54:49 +0200

    лвл 3 1
    +конвеєр і ящики

---
+++ Assets/Scripts/Game Mechanics/ConveyorController.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ConveyorController : MonoBehaviour {
+
+    private SurfaceEffector2D surfaceEffector;
+    private Animator anim;
+    private float speed;
+
+    
+	void Start ()
+    {
+        surfaceEffector = GetComponent<SurfaceEffector2D>();
+        anim = GetComponent<Animator>();
+        SetSpeed();
+	}
+
+    void SetSpeed()
+    {
+        speed = surfaceEffector.speed;
+        anim.SetFloat("speed", speed);
+    }
+}

2476b50e60314ba2530e13fda97aaf2f8aec3433
Blind56 <petrinko6@gmail.com>
2016-02-17 21:42:30 +0200

    багфікс
    забаговане юай і парочка на лвлах

--- Assets/Scripts/Utilities/OptionsManager.cs
+++ Assets/Scripts/Utilities/OptionsManager.cs
@@ -1,4 +1,5 @@
 ﻿using UnityEngine;
+using UnityEngine.UI;
 
 public class OptionsManager : MonoBehaviour {
 
@@ -14,6 +15,7 @@ public class OptionsManager : MonoBehaviour {
         {
             screenWidth = screenHeight * ((float)Screen.width / Screen.height);
             Screen.SetResolution((int)screenWidth, (int)screenHeight, true, 60);
+            GetComponent<UIManager>().UI.GetComponent<CanvasScaler>().referenceResolution = new Vector2(Screen.currentResolution.width, Screen.currentResolution.height);
         }
         if (doCap) { Application.targetFrameRate = frameRate; }
 	}

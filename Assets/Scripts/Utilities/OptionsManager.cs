88a4ffae7c47499eae1619a7bf6622226ce5eff1
Blind56 <petrinko6@gmail.com>
2016-02-20 00:40:14 +0200

    ЮАЙ КРИВОРУК

--- Assets/Scripts/Utilities/OptionsManager.cs
+++ Assets/Scripts/Utilities/OptionsManager.cs
@@ -1,5 +1,6 @@
 ﻿using UnityEngine;
 using UnityEngine.UI;
+using System.Collections;
 
 public class OptionsManager : MonoBehaviour {
 
@@ -8,17 +9,26 @@ public class OptionsManager : MonoBehaviour {
     public bool doChangeResolution;
     public int frameRate;
 
+    private CanvasScaler cs;
     private float screenWidth;
     
 	void Start () {
+        cs = GetComponent<UIManager>().UI.GetComponent<CanvasScaler>();
         if (doChangeResolution)
-        {
-            screenWidth = screenHeight * ((float)Screen.width / Screen.height);
-            Screen.SetResolution((int)screenWidth, (int)screenHeight, true, 60);
-            GetComponent<UIManager>().UI.GetComponent<CanvasScaler>().referenceResolution = new Vector2(Screen.currentResolution.width, Screen.currentResolution.height);
+        {            
+			StartCoroutine (ChangeREsolution ());
         }
         if (doCap) { Application.targetFrameRate = frameRate; }
-	}
+    }
 
-    
+	IEnumerator ChangeREsolution()
+	{
+		Vector2 prevRes = new Vector2 (Screen.width, Screen.height);
+		screenWidth = screenHeight * ((float)Screen.width / Screen.height);
+		Screen.SetResolution((int)screenWidth, (int)screenHeight, true, 0);
+		yield return new WaitForEndOfFrame ();
+		yield return new WaitForEndOfFrame ();
+		Vector2 resDelta = new Vector2(Screen.width - prevRes.x, Screen.height - prevRes.y);
+		cs.referenceResolution -= resDelta;
+	}
 }

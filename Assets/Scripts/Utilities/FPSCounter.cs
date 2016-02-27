6dab7a5ba5b4f2278eb70abbcdc26ef855fcca8c
Blind56 <petrinko6@gmail.com>
2016-02-27 17:45:43 +0200

    FPS
    робить тільки в девбілді

---
+++ Assets/Scripts/Utilities/FPSCounter.cs
@@ -0,0 +1,29 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+
+public class FPSCounter : MonoBehaviour {
+
+    private Text text;
+    private float deltaTime = 0;
+    private float fps;
+    private string fpsText;
+
+	// Use this for initialization
+	void Start ()
+    {
+#if UNITY_EDITOR
+#elif UNITY_ANDROID
+        if (!Debug.isDebugBuild) { gameObject.SetActive(false); }
+#endif
+        text = GetComponent<Text>();
+    }
+	
+	// Update is called once per frame
+	void Update ()
+    {
+        deltaTime += (Time.deltaTime - deltaTime) * 0.1f;
+        fps = 1.0f / deltaTime;
+        fpsText = string.Format("{0:0.} fps", fps);
+        text.text = fpsText;
+    }
+}

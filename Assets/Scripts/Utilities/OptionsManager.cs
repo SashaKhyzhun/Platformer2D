327eb9f2f80d134751c67a11c7d98c3ff996e9c9
Blind56 <petrinko6@gmail.com>
2016-02-29 00:31:56 +0200

    bugfix
    я надеюсь

--- Assets/Scripts/Utilities/OptionsManager.cs
+++ Assets/Scripts/Utilities/OptionsManager.cs
@@ -23,12 +23,15 @@ public class OptionsManager : MonoBehaviour {
 
 	IEnumerator ChangeREsolution()
 	{
-		Vector2 prevRes = new Vector2 (Screen.width, Screen.height);
-		screenWidth = screenHeight * ((float)Screen.width / Screen.height);
-		Screen.SetResolution((int)screenWidth, (int)screenHeight, true, 0);
-		yield return new WaitForEndOfFrame ();
-		yield return new WaitForEndOfFrame ();
-		Vector2 resDelta = new Vector2(Screen.width - prevRes.x, Screen.height - prevRes.y);
+        if (Screen.height > screenHeight)
+        {
+            Vector2 prevRes = new Vector2(Screen.width, Screen.height);
+            screenWidth = screenHeight * ((float)Screen.width / Screen.height);
+            Screen.SetResolution((int)screenWidth, (int)screenHeight, true, 0);
+            yield return new WaitForEndOfFrame();
+            yield return new WaitForEndOfFrame();
+            Vector2 resDelta = new Vector2(Screen.width - prevRes.x, Screen.height - prevRes.y);
+        }
 		//cs.referenceResolution -= resDelta; //fix for broken canvas scaler 
 	}
 }
